522d0f47701de75788a101d480a2990b3f8fe238
liyilin111 <19035327@myrp.edu.sg>
2021-09-18 16:28:09 -0700

    1st commit

---
+++ cw1.codemgmt/src/main/java/com/sddevops/cw1/codemgmt/App.java
@@ -0,0 +1,13 @@
+package com.sddevops.cw1.codemgmt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
